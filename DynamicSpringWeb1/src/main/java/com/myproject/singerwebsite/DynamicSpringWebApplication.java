321bc668fd5a502635e170d75678254f5730086d
Raf <raf.gasparyan.17@mail.ru>
2024-11-13 13:55:07 +0400

    Changes commit

--- DynamicSpringWeb1/src/main/java/com/myproject/singerwebsite/DynamicSpringWebApplication.java
+++ DynamicSpringWeb1/src/main/java/com/myproject/singerwebsite/DynamicSpringWebApplication.java
@@ -1,8 +1,13 @@
 package com.myproject.singerwebsite;
 
+import me.paulschwarz.springdotenv.DotenvApplicationInitializer;
+import me.paulschwarz.springdotenv.DotenvConfig;
+import me.paulschwarz.springdotenv.DotenvPropertyLoader;
+import me.paulschwarz.springdotenv.DotenvPropertySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
@@ -12,13 +17,15 @@ public class DynamicSpringWebApplication {
 //    private EmailSenderService senderService;
 
     public static void main(String[] args) {
-        SpringApplication.run(DynamicSpringWebApplication.class, args);
+        new SpringApplicationBuilder(DynamicSpringWebApplication.class)
+                .initializers(new DotenvApplicationInitializer())
+                .run(args);
 
-    }
+
 //    @EventListener(ApplicationReadyEvent.class)
 //    public void sendMail(){
 //        senderService.sendEmail("raf.gasparyan.15@icloud.com",
 //                "This is Subject",
 //                "This is Body of Email");
-//    }
+    }
 }
